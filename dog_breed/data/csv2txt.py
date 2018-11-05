0a7cebdf30b8682de3e7b0e0451e2cfba2ada3bf
v_王超3 <v_wangchao3@xdf.cn>
2018-11-05 18:53:25 +0800

    数据集转换

--- dog_breed/data/csv2txt.py
+++ dog_breed/data/csv2txt.py
@@ -1,30 +1,36 @@
+# -*- coding:utf8 -*-
 import pandas as pd
 import os
 
 labels = pd.read_csv(r'E:\code\rookie\dog_breed\data\labels.csv')
+
 breed = labels['breed'].unique()
 breed_count = len(breed)
 print breed_count
 breed_dict = {}
+record = {}
 
 train_dir = 'E:/code/rookie/dog_breed/data/train/'
 val_dir = 'E:/code/rookie/dog_breed/data/val/'
 val_path = os.listdir(val_dir)
-print type(val_path[0].split('.')[0])
-print type(labels['id'][0])
 train_path = os.listdir(train_dir)
 
+# 建立不同种类的狗对应的索引字典
 for i in range(len(breed)):
     breed_dict[breed[i]] = i
 for i in range(len(labels)):
     labels['breed'][i] = breed_dict[labels['breed'][i]]
-print labels.head()
-print breed_dict
+    record[labels['id'][i]] = labels['breed'][i]
 
 with open('val.txt', 'a+') as f:
-    for i, val in enumerate(val_path):
-        f.write((str(val) + '\t' + str(breed_dict[val.split('.')[0]]) + '\n'))
+    for val in val_path:
+        f.write((str(val) + ' ' + str(record[val.split('.')[0]]) + '\n'))
 
-with open('label1.txt', 'a+') as f:
-    for line in labels.values:
-        f.write((str(line[0]) + '\t' + str(line[1]) + '\n'))
+with open('train.txt', 'a+') as f:
+    for train in train_path:
+        f.write((str(train) + ' ' + str(record[train.split('.')[0]]) + '\n'))
+
+# with open('label1.txt', 'a+') as f:
+#     for line in labels.values:
+#         f.write((str(line[0]) + '\t' + str(line[1]) + '\n'))
+
